c37d293545af9ad7a2db922fc918f5f3446a88d0
kody <kody.wen@hotmail.com>
2022-01-22 16:50:25 +0000

    fix renew

--- scripts/renew.sh
+++ scripts/renew.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-cat ../refs/blocklist-ipsets/*.*set|grep -v "#" > hol.tmp.txt
-iprange hol.tmp.txt ../refs/zzz/ipset.txt ../refs/zzz/netset.txt > tmp.txt
+mkdir output
+cat refs/blocklist-ipsets/fire*.*set|grep -v "#" > hol.tmp.txt
+cat refs/ipsum/ipsum.txt | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1 > ipsum.tmp.txt
+iprange hol.tmp.txt ipsum.tmp.txt refs/zzz/ipset.txt refs/zzz/netset.txt > tmp.txt
 grep "/" tmp.txt > net.tmp.txt
+iprange net.tmp.txt --optimize > output/netset.txt
+iprange refs/zzz/ipset.txt output/netset.txt > tmp.txt
 grep -v "/" tmp.txt > ip.tmp.txt
-iprange net.tmp.txt --optimize > netset.txt
-iprange ip.tmp.txt --optimize > ipset.txt
+iprange ip.tmp.txt --optimize > output/ipset.txt
 rm *tmp.txt
 
