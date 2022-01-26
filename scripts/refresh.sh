30fa6587ad11fcf61e798d116715eacca1487a7e
kody <kody.wen@hotmail.com>
2022-01-26 08:16:43 +0000

    fix scripts

--- scripts/refresh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ipset flush badnet
-ipset flush badip
-for var in `cat netset.txt`;do ipset add badnet $var;done
-for var in `cat ipset.txt`;do ipset add badip $var;done
-netfilter-persistent save
-
