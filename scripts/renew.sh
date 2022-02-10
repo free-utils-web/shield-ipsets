1fcebf8cc0421f909bd67288da502ba0bee57de6
kody <kody.wen@hotmail.com>
2022-02-10 08:31:17 +0800

    fix renew

--- scripts/renew.sh
+++ scripts/renew.sh
@@ -20,7 +20,7 @@ cat refs/blocklist-ipsets/*prox*.*set|grep -v "#" >> hol.tmp.txt
 cat refs/blocklist-ipsets/firehol_webclient.*set|grep -v "#" >> hol.tmp.txt
 cat refs/blocklist-ipsets/*fraud*.*set|grep -v "#" >> hol.tmp.txt
 cat refs/ipsum/ipsum.txt | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1 > ipsum.tmp.txt
-iprange hol.tmp.txt ipsum.tmp.txt refs/zzz/ipset.txt refs/zzz/netset.txt > tmp.txt
+iprange hol.tmp.txt ipsum.tmp.txt refs/zzz/ipset.txt refs/zzz/netset.txt --except refs/zzz/white.txt > tmp.txt
 grep "/" tmp.txt > net.tmp.txt
 iprange net.tmp.txt --optimize > output/netset.txt
 iprange ipsum.tmp.txt refs/zzz/ipset.txt output/netset.txt > tmp.txt
