04907a33bfc12e1395bf5a51460bd4be385cb647
kody <kody.wen@hotmail.com>
2022-04-03 16:41:30 +0800

    update zzz

--- scripts/renew.sh
+++ scripts/renew.sh
@@ -2,28 +2,12 @@
 
 git submodule foreach 'git reset --hard;git fetch origin master;git checkout master --force'
 mkdir output
-cat refs/blocklist-ipsets/*abuse*.*set|grep -v "#" > hol.tmp.txt
-cat refs/blocklist-ipsets/*spam*.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/*shield*.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/*bad*.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/*black*.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/*dark*.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/*malic*.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/*attac*.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/*bot*.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/*ads*.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/*ssl*.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/*threat*.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/*tor*.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/*bl.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/*chain*.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/firehol_level2.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/firehol_level3.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/firehol_level4.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/firehol_ano*.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/*prox*.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/firehol_webclient.*set|grep -v "#" >> hol.tmp.txt
-cat refs/blocklist-ipsets/*fraud*.*set|grep -v "#" >> hol.tmp.txt
+echo "" > hol.tmp.txt
+
+for file in $(ls refs/blocklist-ipsets/*.*set | grep -E 'abuse|spam|shield|bad|black|dark|mal|attac|bot|ads|ssl|threat|tor|coin|bl\.|chain|crypto|firehol_level2|firehol_level3|firehol_level4|firehol_ano|rox|firehol_webclient|vir|worm|crim|army|force|fraud|ssh|hphost|nullsecure|talos|blam|greensnow|snort|grey|feodo|taichung|cobra|ponmo|phish|bbcan177|bds_atif|packetmail|et_compromised|graphiclineweb|vxvault|gpf|ranso|urando|php|cleantalk|bambenek|myip|et_block|esentire|reputation|blocklist_de|blocklist_net_ua')
+  do cat $file | grep -v "#" >> hol.tmp.txt
+done
+
 cat refs/ipsum/ipsum.txt | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1 > ipsum.tmp.txt
 iprange hol.tmp.txt ipsum.tmp.txt refs/zzz/ipset.txt refs/zzz/netset.txt --except refs/zzz/white.txt > tmp.txt
 grep "/" tmp.txt > net.tmp.txt
