d5e60724bd49b2090caa48cda991e88b378abe9e
oybf5 <oybf5@outlook.com>
2022-08-17 00:35:07 +0800

    update zzz

--- scripts/append.sh
+++ scripts/append.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-iprange output/web.badip.txt output/ssh.badip.txt output/ssh.badnet.txt refs/zzz/ipset.txt refs/zzz/netset.txt > zzz.tmp.txt
+iprange output/web.badip.txt output/ssh.badip.txt output/ssh.badnet.txt refs/zzz/ipset.txt refs/zzz/netset.txt --except output/white.txt > zzz.tmp.txt
 iprange zzz.tmp.txt --optimize > zzz.tmp.o.txt
 grep "/" zzz.tmp.o.txt > refs/zzz/netset.txt
 grep -v "/" zzz.tmp.o.txt > refs/zzz/ipset.txt
